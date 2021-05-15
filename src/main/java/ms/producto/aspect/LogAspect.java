dd96f549382ba9a10e7f36c60319da1986aedf50
sfrigieri <santiagofrigieri@outlook.com>
2021-05-15 02:04:06 -0300

    + mod LogAspect

--- src/main/java/ms/producto/aspect/LogAspect.java
+++ src/main/java/ms/producto/aspect/LogAspect.java
@@ -8,27 +8,42 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @Aspect
 @Component
 public class LogAspect {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(LogAspect. class);
-	
-	@Pointcut("execution(* ms.producto.service.MaterialService.*(..))")
-	private void MaterialServiceMethods() {}
-	
+
+	@Pointcut("execution(* ms.producto.service.*.*(..))")
+	private void serviceMethods() {}
+
 	@Pointcut("execution(public * org.springframework.data.jpa.repository.JpaRepository+.*(..))")
 	private void repositoryMethods() {}
-	
+
+	@Before("serviceMethods()")
+	public void logBeforeServiceMethod(JoinPoint jp) {
+		logger.info("Se ejecutará: "+jp.getTarget().getClass()+"."+jp.getSignature().getName()+Arrays.toString(jp.getArgs()));
+	}
+
 	@Before("repositoryMethods()")
-	public void logBefore(JoinPoint jp) {
-		logger.info("Se ejecutará: "+jp.getSignature().getName()+Arrays.toString(jp.getArgs()));
+	public void logBeforeRepositoryMethod(JoinPoint jp) {
+		logger.info("Se ejecutará repositoryMethod: "+jp.getSignature().getName()+Arrays.toString(jp.getArgs()));
+		
+	}
+
+	@AfterThrowing(pointcut = "serviceMethods() || repositoryMethods()", throwing = "ex")
+	public void logAfterThrowing(JoinPoint jp, Exception ex) {
+		if(ex instanceof DataIntegrityViolationException)
+			logger.info(jp.getSignature().getName()+Arrays.toString(jp.getArgs())+" lanzó excepción: "+((DataIntegrityViolationException) ex).getMostSpecificCause().toString());
+		else
+			logger.info(jp.getSignature().getName()+Arrays.toString(jp.getArgs())+" lanzó excepción: "+ex.getMessage());
 	}
 	
-//	@Before("MaterialServiceMethods()")
-//	public void logBefore(JoinPoint jp) {
-//		logger.info("Se ejecutará: "+jp.getSignature().getName());
-//	}
+	
+
 }
